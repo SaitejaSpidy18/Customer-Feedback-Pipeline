5a4cfd9960d82374e87fe2659074c24b91ae0bd4
Sai Teja <saitejaphani18@gmail.com>
2026-02-19 16:27:23 +0530

    Add script to test feedback API and sentiment flow

---
+++ test.ps1
@@ -0,0 +1,10 @@
+﻿# Test POST feedback
+\ = @{
+    customer_id = 'CUST001'
+    feedback_text = 'This product is absolutely amazing! I love it.'
+    timestamp = (Get-Date).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ')
+} | ConvertTo-Json
+Invoke-RestMethod -Uri 'http://localhost:5000/feedback' -Method Post -Body \ -ContentType 'application/json'
+# Test GET by sentiment after worker processes
+Start-Sleep -Seconds 10
+Invoke-RestMethod -Uri 'http://localhost:5000/feedback?sentiment=positive&limit=5'
